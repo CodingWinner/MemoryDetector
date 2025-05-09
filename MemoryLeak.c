bb162c7f81c6b50cfe6ec5578f9c114f7f508869
EJain-Dev <ekansh.jain2011@gmail.com>
2025-05-09 19:41:19 -0400

    Made code much more readable and slightly faster.

--- MemoryLeak.c
+++ MemoryLeak.c
@@ -1,105 +1,126 @@
 #include <ctype.h>
+#include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+inline char *readFile(const char *name, int *len) {
+  FILE *file;
+  errno_t err = fopen_s(&file, name, "r");
+  if (!file) {
+    printf("Failed to open file with errno code : %i", err);
+    return NULL;
+  }
+  fseek(file, 0, SEEK_END);
+  *len = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char *data = malloc(*len + 1);
+  *len = fread(data, 1, *len, file);
+  fclose(file);
+  data = realloc(data, *len + 1);
+  data[*len] = '\0';
+
+  return data;
+}
+
+inline int wordMatch(const char *file, int *pos, const int len,
+                     const char *string, const int str_len) {
+  if (*pos + str_len >= len) {
+    *pos = len;
+    return 0;
+  }
+  for (int i = 0; i < str_len; i++) {
+    if (string[i] != file[*pos + i]) {
+      return 0;
+    }
+  }
+  *pos += str_len;
+  return 1;
+}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     printf("Need only filepath as arg.");
     return 1;
   } else {
-    FILE *file;
-    errno_t err = fopen_s(&file, argv[1], "r");
-    if (!file) {
-      printf("Failed to open file with errno code : %i", err);
+    int len;
+    char *data = readFile(argv[1], &len);
+    if (data == NULL) {
       return 1;
     }
-    fseek(file, 0, SEEK_END);
-    long len = ftell(file);
-    fseek(file, 0, SEEK_SET);
-    char *data = malloc(len + 1);
-    len = fread(data, 1, len, file);
-    fclose(file);
-    data = realloc(data, len + 1);
-    data[len] = '\0';
+    // Constants of the memory associated functions
+    const char mallocName[] = "malloc";
+    const char callocName[] = "calloc";
+    const char freeName[] = "free";
+
+    // Variable setup
     long numVars = 1;
     char **vars = malloc(sizeof(char *) * numVars);
-    short *freed = malloc(sizeof(int) * numVars);
-    long pos = 0;
+    char *freed = malloc(sizeof(char) * numVars);
+    int pos = 0;
+
+    // Loop through the file
     while (pos < len) {
-      int ogPos;
-      if (data[pos] == 'm') {
-        ogPos = pos - 1;
-        pos++;
-        if (data[pos] == 'a') {
-          pos++;
-          if (data[pos] == 'l') {
-            pos++;
-            if (data[pos] == 'l') {
-              pos++;
-              if (data[pos] == 'o') {
-                pos++;
-                if (data[pos] == 'c') {
-                  pos++;
-                  while (!isalpha(data[ogPos])) {
-                    ogPos--;
-                  }
-                  int startPos = ogPos - 1;
-                  while (isalpha(data[startPos])) {
-                    startPos--;
-                  }
-                  int varLen = ogPos - startPos;
-                  numVars++;
-                  vars = realloc(vars, sizeof(char *) * numVars);
-                  freed = realloc(freed, sizeof(int) * numVars);
-                  freed[numVars - 1] = 0;
-                  vars[numVars - 1] = malloc(sizeof(char) * (varLen + 1));
-                  vars[numVars - 1][varLen] = '\0';
-                  startPos++;
-                  int vc = 0;
-                  while (vc != varLen) {
-                    vars[numVars - 1][vc] = data[startPos];
-                    vc++;
-                    startPos++;
-                  }
-                }
-              }
-            }
-          }
+      int endPos = pos - 1;
+      // Is it malloc or calloc
+      if (wordMatch(data, &pos, len, mallocName, sizeof(mallocName) - 1) ||
+          wordMatch(data, &pos, len, callocName, sizeof(callocName) - 1)) {
+        // Get the end position of the variable name
+        while (!isalpha(data[endPos])) {
+          endPos--;
         }
+
+        // Get the start position of the variable name
+        int startPos = endPos - 1;
+        while (isalpha(data[startPos])) {
+          startPos--;
+        }
+
+        // Create memory for the variable
+        int varLen = endPos - startPos;
+        int varIdx = numVars;
+        numVars++;
+        vars = realloc(vars, sizeof(char *) * numVars);
+        freed = realloc(freed, numVars);
+        vars[varIdx] = malloc(varLen + 1);
+        vars[varIdx][varLen] = '\0';
+
+        // Copy the variable name and set it to unfreed
+        memcpy(vars[varIdx], data + startPos + 1, varLen);
+        freed[varIdx] = 0;
       }
-      if (data[pos] == 'f') {
-        pos++;
-        if (data[pos] == 'r') {
-          pos++;
-          if (data[pos] == 'e') {
-            pos++;
-            if (data[pos] == 'e') {
-              pos += 2;
-              ogPos = pos;
-              while (isalpha(data[pos])) {
-                pos++;
-              }
-              int varLen = pos - ogPos;
-              char *freedVar = malloc(sizeof(char) * (varLen + 1));
-              freedVar[varLen] = '\0';
-              int varPos = 0;
-              while (ogPos != pos) {
-                freedVar[varPos] = data[ogPos];
-                ogPos++;
-                varPos++;
-              }
 
-              for (int i = 1; i < numVars; i++) {
-                if (strcmp(freedVar, vars[i]) == 0) {
-                  freed[i] = 1;
-                }
-              }
+      // Check if there is a call to free
+      if (wordMatch(data, &pos, len, freeName, sizeof(freeName) - 1)) {
+        // Get the start and end positions of the variable being freed
+        int startPos = pos;
+        while (!isalpha(data[startPos])) {
+          startPos++;
+        }
+        endPos = startPos + 1;
+        while (isalpha(data[endPos])) {
+          endPos++;
+        }
 
-              free(freedVar);
-            }
+        // Get the variable name
+        int varLen = endPos - startPos;
+        char *freedVar = malloc(varLen + 1);
+        freedVar[varLen] = '\0';
+        memcpy(freedVar, data + startPos, varLen);
+
+        // Check if it matches existing variable names
+        for (int i = 1; i < numVars; i++) {
+          // If variable is the same as the one freed, set it to freed
+          if (freed[i]) {
+            continue;
+          }
+          if (strcmp(freedVar, vars[i]) == 0) {
+            freed[i] = 1;
+            break;
           }
         }
+
+        free(freedVar);
       }
       pos++;
     }
